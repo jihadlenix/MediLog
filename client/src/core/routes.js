bc3a937d9e9cc72ca265718e4efd79731cbd5d0c
AyaSAbd <163187953+AyaSAbd@users.noreply.github.com>
2025-03-07 02:40:30 +0200

    medications page

--- client/src/core/routes.js
+++ client/src/core/routes.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../auth/LogIn";
 import Signup from "../auth/signup";
 import Dashboard from '../patient/Dashboard';
+import LeftNavBar from '../components/LeftNavBar';
+import Medications from '../patient/Medications';
+
 
 const AppRoutes = () => {
   return (
@@ -11,6 +14,10 @@ const AppRoutes = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashPatient" element={<Dashboard />} />
+      <Route path="/nav" element={< LeftNavBar/>} />
+      <Route path="/medications" element={< Medications/>} />
+
+
     </Routes>
   );
 };
