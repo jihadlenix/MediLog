29a643d7c1acc1c34e09acf827be76dcc3ab330f
Yahya-Akel <akelyahya89@gmail.com>
2025-03-10 23:28:55 +0200

    Save progress on routes.js before switching branches

--- client/src/core/routes.js
+++ client/src/core/routes.js
@@ -3,6 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../auth/LogIn";
 import Signup from "../auth/signup";
 import Dashboard from '../patient/Dashboard';
+import MedRecord from '../patient/MedRecord'
+import DocProfile from "../doctor/docProfile";
+import LeftNavBar from '../components/LeftNavBar';
+import Medications from '../patient/Medications';
 
 const AppRoutes = () => {
   return (
@@ -11,8 +15,14 @@ const AppRoutes = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashPatient" element={<Dashboard />} />
+      <Route path="/medRecord" element={<MedRecord />} />
+      <Route path="/docProfile" element={<DocProfile />} />
+      <Route path="/nav" element={< LeftNavBar />} />
+      <Route path="/medications" element={< Medications />} />
     </Routes>
   );
 };
 
 export default AppRoutes;
+
+
