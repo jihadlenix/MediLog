8eb08dc858141af5cb83d79edcc33f8d5d5980ef
jihadlenix <jihadismasarani@gmail.com>
2025-03-19 04:37:26 +0200

    added new backend with mongodb

---
+++ medilog/src/test/java/com/medilog/medilog/MedilogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilog.medilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
