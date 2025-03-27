f6d6c293a2ea980b0872457074e45cf5d3957687
AyaSAbd <163187953+AyaSAbd@users.noreply.github.com>
2025-03-27 03:01:45 +0200

    sign up fetch

--- backend/src/main/java/com/medilog/medilog/controllers/PatientController.java
+++ backend/src/main/java/com/medilog/medilog/controllers/PatientController.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/patients")
 public class PatientController {
